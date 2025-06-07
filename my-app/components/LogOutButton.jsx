475c8e3df53e6c8e12c01ff2b22dae97867c1bc9
Yazan Jumaa <ys2110319@qu.edu.qa>
2025-06-08 00:35:05 +0300

    Update the log out button

--- my-app/components/LogOutButton.jsx
+++ my-app/components/LogOutButton.jsx
@@ -3,14 +3,51 @@
 import React, { useState } from 'react'
 import { Button } from './ui/button'
 import { Loader2 } from 'lucide-react'
+import { toast } from 'sonner';
+import { useRouter } from 'next/router';
 
 function LogOutButton() {
 
-    const [loading, setLoading] = useState(false);
+    const router = useRouter();
+
+    const [loading, setLoading] = useState(true);
+
+    const handleLogOut = async () => {
+        setLoading(true);
+
+        await new Promise((resolve) => setTimeout(resolve, 2000));
+
+        const errorMessage = null;
+
+        // if there is no error
+
+        if (!errorMessage) {
+            toast.success("Logged out", {
+                    description: "You have been logged out successfully."
+                });
+            router.push("/");
+        }
+
+        // if there is an error
+
+        else {
+            toast.error("Logout failed", {
+                description: errorMessage,
+            });
+        }
+
+        setLoading(false);
+
+        console.log('Logging out...');
+    }
 
   return (
-    <Button>
-        {loading ? <Loader2 className='animate-spin' /> : "Log out"}
+    <Button className="w-24"
+    variant="outline"
+    onClick={handleLogOut}
+    disabled={loading}
+    >
+        {loading ? <Loader2 className='animate-spin' /> : "Log Out"}
     </Button>
   );
 }
