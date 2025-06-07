90c2f18c425a137c59d76d9bd0d8c783ae26e1b5
Yazan Jumaa <ys2110319@qu.edu.qa>
2025-06-07 13:38:54 +0300

    Add Login button

--- my-app/components/Header.jsx
+++ my-app/components/Header.jsx
@@ -2,14 +2,40 @@ import Link from 'next/link'
 import React from 'react'
 import Image from 'next/image';
 import { shadow } from '@/app/styles/utils';
+import { Button } from '@/components/ui/button.jsx';
 
 function Header() {
+  const user = null;
   return (
     <header className='relative flex h-24 w-full items-center justify-between bg-popover px-3 sm:px-8' 
     style={{ boxShadow: shadow }}>
-        <Link href = "/"><Image src = "/goatius.png" height={60} width={60} alt = "GOAT Notes Logo" className="rounded-full"
-        priority></Image>
+        <Link href = "/" className='flex items-end gap-2'>
+          <Image src = "/goatius.png" height={60} width={60} alt = "GOAT Notes Logo" className="rounded-full"
+          priority>
+          </Image>
+          
+          <h1 className='flex flex-col pb-1 text-2xl font-semibold leading-6'>
+            GOAT <span>Notes</span>
+          </h1>
+
         </Link>
+
+        <div className='flex gap-4'>
+
+          {user ? (
+            "Logout"
+          ) :
+          (
+            <>
+              <Button asChild>
+                  <Link href='/login'>Login</Link>
+              </Button>
+            </>
+          )
+        }
+
+        </div>
+
     </header>
   );
 }
