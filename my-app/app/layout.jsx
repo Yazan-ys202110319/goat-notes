514379de66edd828eb9f75bc208270414c9008ca
Yazan Jumaa <ys2110319@qu.edu.qa>
2025-06-07 13:17:48 +0300

    fix page.jsx

--- my-app/app/layout.jsx
+++ my-app/app/layout.jsx
@@ -19,11 +19,11 @@ export default function RootLayout({ children }) {
             >
               <div className="flex min-h-screen w-full flex-col">
                 <Header />
+                <main className="flex flex-1 flex-col px-4 pt-10 xl:px-8">
+                  {children}
+                </main>
+                < Toaster />
               </div>
-              <min className="flex flex-1 flex-col px-4 pt-10 xl:px-8">
-                {children}
-              </min>
-              < Toaster />
             </ThemeProvider>
         </body>
       </html>
